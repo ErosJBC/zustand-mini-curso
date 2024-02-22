399b38897b30381fe8d4e61513fc1bc88a9d27b3
ErosJBC <ebazanc@uni.pe>
2024-02-22 15:05:13 -0500

    Aprendiendo a usar zustand

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -5,53 +5,53 @@ import { AuthLayout, DashboardLayout } from '../layouts';
 import { BearPage, Dashboard, JiraPage, LoginPage, PersonPage, WeddingInvitationPage } from '../pages';
 
 
-export const router = createBrowserRouter( [
-  {
-    path: '/',
-    element: <Root />,
-    children: [
-      /// Dashboard Routes
-      {
-        path: 'dashboard',
-        element: <DashboardLayout />,
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Root />,
         children: [
-          {
-            path: '',
-            element: <Dashboard />
-          },
-          {
-            path: 'bears',
-            element: <BearPage />
-          },
-          {
-            path: 'person',
-            element: <PersonPage />
-          },
-          {
-            path: 'tasks',
-            element: <JiraPage />
-          },
-          {
-            path: 'wedding-invitation',
-            element: <WeddingInvitationPage />
-          }
+            /// Dashboard Routes
+            {
+                path: 'dashboard',
+                element: <DashboardLayout />,
+                children: [
+                    {
+                        path: '',
+                        element: <Dashboard />
+                    },
+                    {
+                        path: 'bears',
+                        element: <BearPage />
+                    },
+                    {
+                        path: 'person',
+                        element: <PersonPage />
+                    },
+                    {
+                        path: 'tasks',
+                        element: <JiraPage />
+                    },
+                    {
+                        path: 'wedding-invitation',
+                        element: <WeddingInvitationPage />
+                    }
 
-        ]
-      },
+                ]
+            },
 
-      /// Auth Routes
-      {
-        path: 'auth',
-        element: <AuthLayout />,
-        children: [
-          {
-            path: 'login',
-            element: <LoginPage />
-          }
-        ]
+            /// Auth Routes
+            {
+                path: 'auth',
+                element: <AuthLayout />,
+                children: [
+                    {
+                        path: 'login',
+                        element: <LoginPage />
+                    }
+                ]
 
-      },
+            },
 
-    ],
-  },
-] );
+        ],
+    },
+]);
